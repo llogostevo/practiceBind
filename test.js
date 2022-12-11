8f268936d60128c1f105011fcebaeebb48e90e61
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-12-11 09:36:10 +0000

    update test.js with setTimeout call

--- test.js
+++ test.js
@@ -1,6 +1,20 @@
-// Your code here
+// import the employee class
 const Employee = require('./employee.js');
 
+// create instance of Employee(name, occupation)
 const employee = new Employee("John Wick", "Dog Lover");
 
-employee.sayName();
+// create a global function sayName from the employee method say name
+const sayName = employee.sayName;
+
+// create new function with the employee bound to the global function to create
+// a new function sayEmployeeName
+const sayEmployeeName = sayName.bind(employee);
+
+// calling this will output the original method call that is now
+// within this function and bound to employee
+sayEmployeeName()
+
+// this now works in the setTimeout as we have bound the 
+// `this` object to the employee within this global context function
+setTimeout(sayEmployeeName, 1500);
