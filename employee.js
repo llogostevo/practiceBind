17e81779dcb0fae93dd6d13adbb5c3685255ea14
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-12-11 09:25:19 +0000

    create class and test

--- employee.js
+++ employee.js
@@ -6,5 +6,15 @@ class Employee {
         this.name= name; 
         this.occupation= occupation;
     }
+
+    sayName(){
+        console.log(`${this.name} says hello`)
+    }
+
+    sayOccupation(){
+        console.log(`${this.name} is a ${this.occupation}`)
+    }
 }
+
+module.exports = Employee;
 
--- test.js
+++ test.js
@@ -1,1 +1,6 @@
 // Your code here
+const Employee = require('./employee.js');
+
+const employee = new Employee("John Wick", "Dog Lover");
+
+employee.sayName();
